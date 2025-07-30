5017f6e60ddf708eea28faa08a09ea996e1a4433
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-30 13:13:58 -0300

    feat: separando responsabilidades

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {Text, StyleSheet, View} from "react-native";
+import {Text, View} from "react-native";
 import { StatusBar } from 'expo-status-bar';
-
+import styles from "../Home/Style"
 export default function Home() {
     return(
         <View style={styles.container}>
@@ -12,11 +12,4 @@ export default function Home() {
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
